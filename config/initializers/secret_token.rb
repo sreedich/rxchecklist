309eb166d6cd1af1611e0b3514a90d9823e6915b
Steven Reedich <sreedich@gmail.com>
2013-06-14 13:24:39 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RxchecklistApp::Application.config.secret_token = '4f0eba24d9f667cd40ff5f3d34b35e94201c4db35b0d20ba3fc57dae0fc21996ec184df1439fc7577ad1d6d9e5ca2715bc99a37552a7a8283c44e911adef673b'
