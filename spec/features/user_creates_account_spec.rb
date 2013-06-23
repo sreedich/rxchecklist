1d4c983b9aeb04126ec4cbbcaaadbec997808149
Steven Reedich <sreedich@gmail.com>
2013-06-23 18:54:59 -0400

    refactored user creates account

--- spec/features/user_creates_account_spec.rb
+++ spec/features/user_creates_account_spec.rb
@@ -12,7 +12,15 @@ require 'spec_helper'
      # 5) Successfully registered user will be redirected to explores_path
 
 
+
 describe 'user creates an account' do 
+
+# Returns a User instance that's not saved
+let(:user_signup) {FactoryGirl.build(:user)}
+
+# Returns a saved User instance
+let(:user) {FactoryGirl.create(:user)}
+
 
   it 'user visits signup page' do
     visit new_user_registration_path
@@ -21,28 +29,19 @@ describe 'user creates an account' do
 
   it 'allows user to create an account' do
     visit new_user_registration_path
-    fill_in 'Email', with: 'tester@gmail.com'
-    fill_in 'Password', with: '12345678'
-    fill_in 'Password confirmation', with: '12345678'
-    click_on 'Sign up'
+    sign_up_as(user_signup)
     expect(User.count).to eql(1)
   end 
 
   it 'notifies user that account was successfully created' do 
     visit new_user_registration_path
-    fill_in 'Email', with: 'tester@gmail.com'
-    fill_in 'Password', with: '12345678'
-    fill_in 'Password confirmation', with: '12345678'
-    click_on 'Sign up'
+    sign_up_as(user_signup)
     expect(page).to have_content 'Welcome! You have signed up successfully.'
   end 
 
   it 'navigates signed up user to index page' do
     visit new_user_registration_path
-    fill_in 'Email', with: 'tester@gmail.com'
-    fill_in 'Password', with: '12345678'
-    fill_in 'Password confirmation', with: '12345678'
-    click_on 'Sign up'
+    sign_up_as(user_signup)
     expect(current_path).to eql(root_path)
   end 
 
@@ -66,10 +65,7 @@ describe 'user creates an account' do
 
   it 'allows user to log out' do 
     visit new_user_registration_path
-    fill_in 'Email', with: 'tester@gmail.com'
-    fill_in 'Password', with: '12345678'
-    fill_in 'Password confirmation', with: '12345678'
-    click_on 'Sign up'
+    sign_up_as(user_signup)
     click_on 'Logout'
     expect(page).to have_content 'Signed out successfully.'
   end 
