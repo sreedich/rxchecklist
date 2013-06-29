0abc7d530868cc3971106c18f68767aec9568426
Steven Reedich <sreedich@gmail.com>
2013-06-29 18:30:56 -0400

    create see detail for checklist list

--- spec/features/visit_explore_page_spec.rb
+++ spec/features/visit_explore_page_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe 'explore page' do 
 
   let(:user) {FactoryGirl.create(:user)}
-  let(:checklist) {FactoryGirl.create_list(:checklist, 10)}
+  let(:checklists) {FactoryGirl.create_list(:checklist, 10)}
+  let(:checklist) {FactoryGirl.create(:checklist)}
 
   it 'user visits explore page' do 
     visit explore_index_path  
@@ -22,8 +23,16 @@ describe 'explore page' do
 
   it 'user can view a list of checklists' do 
     sign_in_as(user)
-    checklist  
+    checklists 
     visit explore_index_path
+    expect(page).to have_content 'This is an awesome checklist title'
+  end 
+
+  it 'user navigates to checklist show page' do 
+    sign_in_as(user)
+    checklist
+    visit explore_index_path
+    click_link 'See Details'
     expect(page).to have_content 'This is an awesome checklist title'
   end 
 
