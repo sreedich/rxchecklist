4e4fe75b61926125fd5987ad2b755dbe2f377163
Steven Reedich <sreedich@gmail.com>
2013-06-29 18:03:11 -0400

    checklist titles render on explore page

--- spec/features/visit_explore_page_spec.rb
+++ spec/features/visit_explore_page_spec.rb
@@ -3,9 +3,10 @@ require 'spec_helper'
 describe 'explore page' do 
 
   let(:user) {FactoryGirl.create(:user)}
+  let(:checklist) {FactoryGirl.create_list(:checklist, 10)}
 
   it 'user visits explore page' do 
-    visit explore_index_path 
+    visit explore_index_path  
     expect(page).to have_content 'Explore'
     expect(page).to have_content 'Trending'
     expect(page).to have_content 'Featured'
@@ -13,10 +14,17 @@ describe 'explore page' do
   end
 
   it 'user can navigate to new checklist page' do
-    sign_in_as(user) 
+    sign_in_as(user)  
     visit explore_index_path
     click_on 'Create New Checklist'
     expect(page).to have_content 'Checklist Title'
+  end 
+
+  it 'user can view a list of checklists' do 
+    sign_in_as(user)
+    checklist  
+    visit explore_index_path
+    expect(page).to have_content 'This is an awesome checklist title'
   end 
 
 end 
