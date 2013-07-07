a99dd50de3f40b90aa345ed17d86bb1f0470282d
Steven Reedich <sreedich@gmail.com>
2013-07-06 21:19:33 -0400

    refactored password confirmation tests

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -10,7 +10,7 @@ module AuthenticationHelper
     visit new_user_registration_path
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
-    fill_in 'Password confirmation', with: user.password_confirmation 
+    fill_in 'Password Confirmation', with: user.password_confirmation 
     click_button 'Sign up'
   end
 
