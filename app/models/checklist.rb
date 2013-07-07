4d0157163367df2ead0511ba9f97b0abd9a87055
Steven Reedich <sreedich@gmail.com>
2013-07-06 21:58:48 -0400

    add_column description to checklists

--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -1,5 +1,5 @@
 class Checklist < ActiveRecord::Base
-  attr_accessible :title, :user_id, :tasks_attributes 
+  attr_accessible :title, :description, :user_id, :tasks_attributes 
   validates_presence_of :title
   validates_presence_of :user_id
   belongs_to :user
