e2f811a04f43e129f5b83275ea706ea8a253245f
Steven Reedich <sreedich@gmail.com>
2013-07-07 20:33:13 -0400

    styled search on explore page

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -2,7 +2,7 @@ class ExploreController < ApplicationController
   def index
     # @checklists = Checklist.all
     @q = Checklist.search(params[:q])
-    @checklists = @q.result.page(params[:page]).per(10)
+    @checklists = @q.result.page(params[:page]).per(5)
 
   end
 end
